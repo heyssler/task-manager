c265d27c2e3e80139f401e5309536f6976ec6f6e
Eryk Heyssler <eryk@Eryks-MacBook-Pro.local>
2024-04-15 11:38:07 -0600

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import TaskManager from './components/TaskManager';
-import React, { useState, useEffect, Component } from 'react';
+import React from 'react';
 
 
 function App() {
